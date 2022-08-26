48c1e859b9da9b0bb18b75338260f9b0ef8cec1f
Marina <umarishka@bk.ru>
2022-08-26 19:02:03 +0300

    add N15

---
+++ N15/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите цифру, которая соответствует дню недели: ");
+ // 1-monday, 2-tuesday...//
+int a=int.Parse(Console.ReadLine());
+if (a==6 || a==7)
+{
+Console.WriteLine(a + "->" + "Weekend");
+}
+else
+{
+  Console.WriteLine(a + "->" + "Weekday");  
+}
