03443c60bcbc6a733428a962570895fb7570cced
Marina <umarishka@bk.ru>
2022-08-26 18:58:27 +0300

    add N10

---
+++ N10/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите трехзначное число ");
+int a = int.Parse(Console.ReadLine());
+int b = a %100;
+int c = b/10;
+Console.WriteLine("Вторая цифра введенного числа" + "->"+ c); 
